6b5ff5db9894d621b07e08adb10d5b11cf552384
Chris Kennedy <chris.kennedy@crunchyroll.com>
2023-03-26 00:39:07 -0700

    updates for schema

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,92 +1,56 @@
-#!/usr/local/bin/python3
-
 import os
 import json
 import re
-import argparse
-from pathlib import Path
+import sys
 from fpdf import FPDF
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk import download
 
-download("punkt")
 download("stopwords")
+download("punkt")
+
 stop_words = set(stopwords.words("english"))
 
-def remove_urls(text):
-    return re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
-
-def remove_stop_words(text):
+def remove_stop_words_and_links(text):
     word_tokens = word_tokenize(text)
-    filtered_text = [word for word in word_tokens if word.lower() not in stop_words]
+    filtered_text = [w for w in word_tokens if not w in stop_words and not re.match(r"http\S+", w)]
     return " ".join(filtered_text)
 
-def preprocess_message(message):
-    message = remove_urls(message)
-    message = remove_stop_words(message)
-    return message
+def process_conversation(conversation, main_person, output_dir):
+    participants = [p["name"] for p in conversation["participants"]]
+    if main_person in participants:
+        for message in conversation["messages"]:
+            if "content" in message:
+                sender_name = message["sender_name"]
+                cleaned_content = remove_stop_words_and_links(message["content"])
 
-def convert_to_training_data(messenger_data, main_name):
-    training_data = []
+                pdf_filename = os.path.join(output_dir, f"{sender_name}_chatbot.pdf")
+                txt_filename = os.path.join(output_dir, f"{sender_name}_chatbot.txt")
+                pdf = FPDF()
+                pdf.add_page()
+                pdf.set_font("Arial", size=12)
+                pdf.multi_cell(0, 10, txt=cleaned_content)
 
-    for message in messenger_data['messages']:
-        if 'content' in message:
-            preprocessed_content = preprocess_message(message['content'])
-            if message['sender_name'] == main_name:
-                prompt = f"Bot: {preprocessed_content}"
-            else:
-                prompt = f"{message['sender_name']}: {preprocessed_content}"
-            completion = f"{message['sender_name']} says: {preprocessed_content}"
-            training_data.append({"prompt": prompt, "completion": completion})
+                with open(txt_filename, "a", encoding="utf-8") as txt_file:
+                    txt_file.write(cleaned_content + "\n\n")
 
-    return training_data
+                pdf.output(pdf_filename)
 
-def generate_pdf(conversations, output_path, main_name):
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
+def main(messenger_data_dir, main_person):
+    output_dir = "output"
+    os.makedirs(output_dir, exist_ok=True)
 
-    for conversation in conversations:
-        if conversation['prompt'].startswith(f"Bot: "):
-            pdf.set_text_color(0, 0, 255)  # Set text color to blue for the main person (Bot)
-        else:
-            pdf.set_text_color(0, 0, 0)  # Set text color to black for others
-        pdf.cell(200, 10, txt=conversation['prompt'], ln=True)
-
-    pdf.output(output_path)
-
-def main(directory, main_name):
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.startswith("message_") and file.endswith(".json"):
-                print("Processing file: %s" % file)
-                file_path = os.path.join(root, file)
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    messenger_data = json.load(f)
-
-                # Check if main_name is in the conversation
-                participants = {p['name'] for p in messenger_data['participants']}
-                print(participants)
-                if main_name not in participants:
-                    continue
-
-                training_data = convert_to_training_data(messenger_data, main_name)
-                output_directory = Path("output")
-                output_directory.mkdir(parents=True, exist_ok=True)
-
-                for participant in participants:
-                    if participant == main_name:
-                        output_path = output_directory / f"{participant}_main_bot.pdf"
-                    else:
-                        output_path = output_directory / f"{participant}.pdf"
-                    generate_pdf(training_data, output_path, main_name)
+    for root, dirs, files in os.walk(messenger_data_dir):
+        for filename in files:
+            if filename.startswith("message_") and filename.endswith(".json"):
+                file_path = os.path.join(root, filename)
+                with open(file_path, encoding="utf-8") as f:
+                    conversation = json.load(f)
+                    process_conversation(conversation, main_person, output_dir)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process Facebook Messenger data and generate PDFs.")
-    parser.add_argument("directory", type=str, help="Path to the directory containing Facebook Messenger data.")
-    parser.add_argument("main_name", type=str, help="Name of the main person to be treated as the main bot.")
-    args = parser.parse_args()
-
-    main(args.directory, args.main_name)
+    messenger_data_dir = sys.argv[1]
+    main_person = sys.argv[2]
+    main(messenger_data_dir, main_person)
 
